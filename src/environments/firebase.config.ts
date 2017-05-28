bd60f47036f08e3dc93e45dc306f0e15bf83dccd
baesparza <bruno.be81@gmail.com>
2017-05-28 17:18:38 -0500

    Added firebase and angularfire2

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyB6Tcc3ToXqY7H-719RATTfp7YOlymEpEU',
+    authDomain: 'firechat-87a67.firebaseapp.com',
+    databaseURL: 'https://firechat-87a67.firebaseio.com',
+    projectId: 'firechat-87a67',
+    storageBucket: 'firechat-87a67.appspot.com',
+    messagingSenderId: '575230720393'
+};
