5a8eb0768624bc184d9591bc5deb82e1dd1ade5d
baesparza <bruno.be81@gmail.com>
2017-05-28 21:41:50 -0500

    Delleted some trash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +7,7 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class AppComponent {
 
-  chats: FirebaseListObservable<any[]>;
+   constructor() { }
 
-  constructor(_angularFireDatabase: AngularFireDatabase) {
-
-    this.chats = _angularFireDatabase.list('/chats');
-
-  }
 
 }
