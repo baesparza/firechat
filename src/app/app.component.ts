de4508a33f144d3d753d6bdd5d4e0b5144197646
baesparza <bruno.be81@gmail.com>
2017-05-28 19:18:07 -0500

    angularfire2 working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  chats: FirebaseListObservable<any[]>;
+
+  constructor(_angularFireDatabase: AngularFireDatabase) {
+
+    this.chats = _angularFireDatabase.list('/chats');
+
+  }
+
 }
